ce57b6970da5eb2cb6cdd0a24133ac5ea43d4b7b
amir.mardani <amir.mardani@autotrader.co.uk>
2017-03-14 22:14:24 +0000

    setup email address

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,7 +17,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://formspree.io/info@damnooshkohandiyar.com",
                 type: "POST",
                 data: {
                     name: name,
@@ -31,7 +31,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>پیغام شما با موفقیت ارسال شد</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -43,7 +43,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>متاسفانه پیغام شما ارسال نشد");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
